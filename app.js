d9ef5b2b56efa0cbed8e983d9a317f09db41b94d
KRVaghani <kaushik.vaghani111@gmail.com>
2021-02-01 11:34:29 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const request = require('request');
+
+fs.readFile('movie.txt',(err,query)=>{
+    if(err) return console.log(err);
+
+    request('https://api.themoviedb.org/3/search/movie?api_key=fbfdf6274fb627cb59862300a89f7265&query='+query.toString(),{timeout:0},(err,response,body)=>{
+        if(err) return console.log(err);
+
+        let movies = JSON.parse(body);
+
+        movies.results.forEach((movie)=>{
+            console.log(movie.original_title);
+        })
+    })
+})
